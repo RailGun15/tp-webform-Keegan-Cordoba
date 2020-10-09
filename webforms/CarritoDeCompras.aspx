b5119228676918e93138cc77d10e32d3f4d6d056
Ignacio1997-prog <63248914+Ignacio1997-prog@users.noreply.github.com>
2020-10-08 23:46:10 -0300

    Nombre e Imagenes de ARTICULOS
    se agrego los nombres e imagenes de los articulos y tambien se agrego una pagina de Carrito de compras

---
+++ webforms/CarritoDeCompras.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Carro De Compras" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CarritoDeCompras.aspx.cs" Inherits="webforms.CarritoDeCompras" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Carrito De Compras</h2>
+
+</asp:Content>
--- webforms/CarritoDeCompras.aspx.cs
+++ webforms/CarritoDeCompras.aspx.cs
@@ -4,19 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Comercio;
-using Dominio;
 
 namespace webforms
 {
-    public partial class _Default : Page
+    public partial class CarritoDeCompras : System.Web.UI.Page
     {
-        public List<Articulo> listaArticulo { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            ArticuloComercio comercio = new ArticuloComercio();
-            listaArticulo = comercio.Listar();
-            
+
         }
     }
 }
